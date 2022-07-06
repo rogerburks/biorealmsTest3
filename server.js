59e09486a2e1d4d971d2bf7292642dfe429a6753
rogerburks <17787203+rogerburks@users.noreply.github.com>
2022-07-05 21:04:22 -0400

    added cors to help deal with local versus remote access

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 8675
+
+app.use(cors())
 
 //The data are contained in the object called realms
 const realms = {
